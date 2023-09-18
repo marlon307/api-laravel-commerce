1234b736fd4694bd24fb38c63eadccf2e298b563
marlon307 <marlon-ramosb@hotmail.com>
2023-09-18 15:26:09 -0300

    FEAT: auth user

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -21,6 +21,9 @@ class Kernel extends HttpKernel
         \Illuminate\Foundation\Http\Middleware\ValidatePostSize::class,
         \App\Http\Middleware\TrimStrings::class,
         \Illuminate\Foundation\Http\Middleware\ConvertEmptyStringsToNull::class,
+        /*  Login */
+        \Illuminate\Session\Middleware\StartSession::class,
+        \Illuminate\View\Middleware\ShareErrorsFromSession::class,
     ];
 
     /**
@@ -40,7 +43,7 @@ class Kernel extends HttpKernel
 
         'api' => [
             // \Laravel\Sanctum\Http\Middleware\EnsureFrontendRequestsAreStateful::class,
-            \Illuminate\Routing\Middleware\ThrottleRequests::class.':api',
+            \Illuminate\Routing\Middleware\ThrottleRequests::class . ':api',
             \Illuminate\Routing\Middleware\SubstituteBindings::class,
         ],
     ];
--- routes/api.php
+++ routes/api.php
@@ -2,5 +2,8 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\UserController;
+use App\Http\Controllers\LoginController;
 
-Route::middleware('auth')->get('user', [UserController::class, 'show']);
+// Route::middleware('auth')->get('user', [UserController::class, 'show']);
+
+Route::post('/user/login', [LoginController::class, 'authenticate']);
